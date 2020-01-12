4294c2a127a6ad206fc3d74a4102cbc7dc92bd7f
Yao Liu <io.yao.liu@gmail.com>
2020-01-11 23:47:50 -0800

    WIP fact check

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
-let analyzeText = document.getElementById("analyzeText");
+let analyzeTextButton = document.getElementById("analyzeTextButton");
 let textToAnaylze = document.getElementById("textToAnalyze")
-let popupContainer = document.getElementById("popupContainer")
 
-analyzeText.onclick = async () => {
+let factCheckTextButton = document.getElementById("factCheckTextButton");
+let textToFactCheck = document.getElementById("textToFactCheck")
+
+analyzeTextButton.onclick = async () => {
     fetch('https://nwhackers2020.appspot.com/analyze', {
       method: 'POST',
       headers: {
@@ -18,3 +20,26 @@ analyzeText.onclick = async () => {
       console.error('Error:', error);
     });
 }
+
+factCheckTextButton.onclick = async () => {
+    fetch('https://nwhackers2020.appspot.com/analyze', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: "text=" + textToAnaylze.value,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+}, function(selection) {
+    document.getElementById("textToFactCheck").value = selection[0];
+});
